389faea6abba123919707f696b084fb2d8166218
unknown <oussemabenbouali.bouyahia@gmail.com>
2025-03-14 10:27:13 +0100

    implement layout and basic blade directives

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,10 @@
-<a {{ $attributes }}>
+
+@props(["active"])
+<a href="/" class="{{ $active?
+ "bg-gray-900 text-white":
+"text-gray-300 hover:bg-gray-700 hover:text-white"}}"
+aria-current="{{ $active?"page":false}}"
+{{ $attributes }}>
     {{ $slot }}
 </a>
+
