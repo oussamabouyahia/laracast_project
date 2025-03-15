886c712994cc3903a04b3242e21415058bd6aefb
unknown <oussemabenbouali.bouyahia@gmail.com>
2025-03-15 13:29:22 +0100

    adding job and employers models

---
+++ app/Models/Employer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employer extends Model
+{
+    /** @use HasFactory<\Database\Factories\EmployerFactory> */
+    use HasFactory;
+    protected $fillable=["name"];
+
+    public function jobs():HasMany{
+        return $this->hasMany(Job::class);
+    }
+}
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,12 +1,20 @@
 <?php
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model{
-
+use HasFactory;
 protected $table="job_listing";
 
 protected $fillable=["title","salary"];
+
+public function employer():BelongsTo {
+   return  $this->belongsTo(Employer::class);
+}
+
 }
 
 ?>
